5d43d153950b09a3c6faec0b5fa944f14c9ab61f
Jeetan Madaan <madajeeblog@gmail.com>
2019-02-19 16:16:33 -0500

    second commit:c2

--- config/index.js
+++ config/index.js
@@ -3,8 +3,10 @@ var configValues = require('./config');
 module.exports = {
     
     getDbConnectionString: function() {
-         return 'mongodb+srv://' + configValues.uname + ':' + configValues.pwd + 
-        '@' + configValues.dburl;
+         /*return 'mongodb+srv://' + configValues.uname + ':' + configValues.pwd + 
+        '@' + configValues.dburl;*/
+        //return configValues.DATABASE_URL_PAAS;
+        return process.env.DATABASE_URL_PAAS;
     }
     
 }
